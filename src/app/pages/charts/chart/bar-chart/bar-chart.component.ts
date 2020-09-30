1e6e6b7b33c1336302d32e399daee65ef209bc01
Ovi <ovidio.reyna@gmail.com>
2020-09-30 16:16:38 -0400

    fixed chart not showing up issue

---
+++ src/app/pages/charts/chart/bar-chart/bar-chart.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { NbThemeService, NbColorHelper } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss'],
+})
+export class BarChartComponent implements OnInit, OnDestroy {
+  @Input() data: any;
+  @Input() options: any;
+  barData: any;
+  barOptions: any;
+  themeSubscription: any;
+
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const chartjs: any = config.variables.chartjs;
+
+      this.options = {
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+          labels: {
+            fontColor: chartjs.textColor,
+          },
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+        },
+      };
+    });
+  }
+
+  ngOnInit() {
+    this.barData = this.data;
+    this.barOptions = this.options;
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
+}
